b4b0b404c7f69a7b2af099c56fd30409a7ec9bfb
James Chan <shotgun8767@gmail.com>
2021-09-26 14:27:56 +0800

    feat: assignment 1.4

--- assignment/1/4.py
+++ assignment/1/4.py
@@ -8,30 +8,50 @@
 
 
 # ---------- [ code ] ----------
+# def solve(arr, target, l, r):
+#     # Conquer
+#     if l == r:
+#         # if the only one element in array equals to target, (0, 0) is returned,
+#         # (-1, -1) is returned otherwise.
+#         return (0, 0) if arr[l] == target else (-1, -1)
+#
+#     # Divides
+#     m = (l + r) >> 1  # medium index
+#     left = solve(arr, target, l, m) if arr[m] >= target else (-1, -1)
+#     right = solve(arr, target, m + 1, r) if arr[m + 1] <= target else (-1, -1)
+#
+#     # Combine
+#     deviation = m - l + 1
+#     if left[0] == -1:
+#         # in this case, left part does not contain target element.
+#         # left == (-1, -1)
+#         return left if right[0] == -1 else (right[0] + deviation, right[1] + deviation)
+#     else:
+#         # in this case, left part contains at least one target element.
+#         return left if right[0] == -1 else (left[0], right[1] + deviation)
+
 def solve(arr, target, l, r):
     # Conquer
     if l == r:
-        # if the only one element in array equals to target, (0, 0) is returned,
-        # (-1, -1) is returned otherwise.
-        return (0, 0) if arr[l] == target else (-1, -1)
+        return (r, r) if arr[r] == target else (-1, -1)
 
     # Divides
-    m = (l + r) >> 2  # middle index
+    m = (l + r) >> 1
     left = solve(arr, target, l, m) if arr[m] >= target else (-1, -1)
-    right = solve(arr, target, m + 1, r)
+    right = solve(arr, target, m + 1, r) if arr[m + 1] <= target else (-1, -1)
 
     # Combine
-    deviation = m - l + 1
     if left[0] == -1:
-        # in this case, left part does not contain target element.
-        # left == (-1, -1)
-        return left if right[0] == -1 else (right[0] + deviation, right[1] + deviation)
+        # in this case, left half does not contain target element.
+        return right
     else:
         # in this case, left part contains at least one target element.
-        return left if right[0] == -1 else (left[0], right[1] + deviation)
+        return left if right[0] == -1 else (left[0], right[1])
+
+    # ---------- [ test ] ----------
 
 
-# ---------- [ test ] ----------
-arr = [5]
-result = solve(arr, 8, 0, len(arr) - 1)
+arr = [5, 7, 7, 8, 8, 10]
+target = 8
+result = solve(arr, target, 0, len(arr) - 1)
 print(result)
